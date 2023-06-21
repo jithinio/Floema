96100f446a81fdb107d0fab8fa2ee2259534ccc7
jithinio <hello@jithin.io>
2023-06-21 09:15:24 +0530

    Changes to app.js migrating to prismic v 7

--- app.js
+++ app.js
@@ -17,6 +17,65 @@ const initApi = (req) => {
   })
 }
 
+const HandleLinkResolver = (doc) => {
+  // Define the url depending on the document type
+  //   if (doc.type === 'page') {
+  //     return '/page/' + doc.uid;
+  //   } else if (doc.type === 'blog_post') {
+  //     return '/blog/' + doc.uid;
+  //   }
+
+  // Default to homepage
+  return '/'
+}
+
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+app.locals.basedir = app.get('views')
+
+const handleRequest = async (api) => {
+  const [meta, home, about, { results: collections }] = await Promise.all([
+    api.getSingle('meta'),
+    api.getSingle('home'),
+    api.getSingle('about'),
+    api.query(Prismic.Predicates.at('document.type', 'collection'), {
+      fetchLinks: 'product.image'
+    })
+  ])
+
+  const assets = []
+
+  home.data.gallery.forEach((item) => {
+    assets.push(item.image.url)
+  })
+
+  about.data.gallery.forEach((item) => {
+    assets.push(item.image.url)
+  })
+
+  about.data.body.forEach((section) => {
+    if (section.slice_type === 'gallery') {
+      section.items.forEach((item) => {
+        assets.push(item.image.url)
+      })
+    }
+  })
+
+  collections.forEach((collection) => {
+    collection.data.products.forEach((item) => {
+      assets.push(item.products_product.data.image.url)
+    })
+  })
+
+  return {
+    assets,
+    meta,
+    home,
+    collections,
+    about
+  }
+}
+
 const handleLinkResolver = doc => {
   if (doc.type === 'product') {
     return `/detail/${doc.uid}`
@@ -34,37 +93,60 @@ const handleLinkResolver = doc => {
 }
 
 app.use((req, res, next) => {
+  res.locals.ctx = {
+    endpoint: process.env.PRISMIC_ENDPOINT,
+    linkResolver: HandleLinkResolver
+  }
+  res.locals.PrismicH = PrismicH
+
+  next()
+})
+
+app.use((req, res, next) => {
   res.locals.Link = handleLinkResolver
   res.locals.PrismicH = PrismicH
   next()
 })
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
-
 app.get('/', async (req, res) => {
-  res.render('pages/home')
-})
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
 
-app.get('/about', async (req, res) => {
-  initApi(req).then(api => {
-    api.query(Prismic.Predicates.any('document.type', ['about', 'meta'])).then(response => {
-      const { results } = response
-      const [about, meta] = results
-      res.render('pages/about', {
-        about,
-        meta
-      })
-    })
+  res.render('pages/home', {
+    ...defaults
   })
 })
 
-app.get('/detail/:id', (req, res) => {
-  res.render('pages/detail')
+app.get('/about', async (req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+
+  res.render('pages/about', {
+    ...defaults
+  })
 })
 
-app.get('/collections', (req, res) => {
-  res.render('pages/collections')
+app.get('/collections', async (req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+
+  res.render('pages/collections', {
+    ...defaults
+  })
+})
+
+app.get('/detail/:uid', async (req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+
+  const product = await api.getByUID('product', req.params.uid, {
+    fetchLinks: 'collection.title'
+  })
+
+  res.render('pages/detail', {
+    ...defaults,
+    product
+  })
 })
 
 app.listen(port, () => {
